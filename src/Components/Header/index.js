41432a6e011d689abc7516812062c5c7ca2504fc
Pavan <pavan.kumar@dakoo.app>
2023-09-29 16:39:34 +0530

    css change

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -25,14 +25,14 @@ const Header = () => {
   const messageColor = currentPath === "/chats" ? "#3B82F6" : "#000";
 
   return (
-    <div className=" flex items-center bg-slate-100 p-2 px-6 mb-2">
-      <Link href="/" className="text-black text-xl font-semibold mr-auto">
+    <div className="header-bg ">
+      <Link href="/" className="text-black text-xl font-semibold mr-auto " style={{marginRight:'auto'}}>
         <h1>Images</h1>
       </Link>
-      <Link href="/profile" className="mr-4">
+      <Link href="/profile" className="mr-4 hov">
         <BsPersonCircle style={{ color: profileColor }} size={26} />
       </Link>
-      <Link href="/chats">
+      <Link href="/chats" className="hov">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           height="1.5em"
@@ -41,12 +41,13 @@ const Header = () => {
           <path
             d="M256.55 8C116.52 8 8 110.34 8 248.57c0 72.3 29.71 134.78 78.07 177.94 8.35 7.51 6.63 11.86 8.05 58.23A19.92 19.92 0 0 0 122 502.31c52.91-23.3 53.59-25.14 62.56-22.7C337.85 521.8 504 423.7 504 248.57 504 110.34 396.59 8 256.55 8zm149.24 185.13l-73 115.57a37.37 37.37 0 0 1-53.91 9.93l-58.08-43.47a15 15 0 0 0-18 0l-78.37 59.44c-10.46 7.93-24.16-4.6-17.11-15.67l73-115.57a37.36 37.36 0 0 1 53.91-9.93l58.06 43.46a15 15 0 0 0 18 0l78.41-59.38c10.44-7.98 24.14 4.54 17.09 15.62z"
             fill={messageColor}
+            
           />
         </svg>
       </Link>
 
       <button
-        className="logout-button border-2 border-blue-500 text-blue-500 font-semibold px-2 rounded-md ml-2  hover:text-white hover:bg-blue-500"
+        className="logout-button color-logout-button"
         onClick={onLogout}
       >
         Logout
