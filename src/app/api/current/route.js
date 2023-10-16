3a56264317156703079407f09ee7d0dde971a647
Pavan <pavan.kumar@dakoo.app>
2023-10-16 22:59:55 +0530

    current

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -1,22 +1,20 @@
-import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
-import User from "@/models/user";
+// import { NextResponse } from "next/server";
+// import jwt from "jsonwebtoken";
+// import User from "@/models/user";
 
-export async function GET(request) {
+// export async function GET(request) {
 
-    const data = jwt.verify(request.cookies.get('jwt_token').value, process.env.JWT_SECRET)
+//     const data = jwt.verify(request.cookies.get('jwt_token').value, process.env.JWT_SECRET)
+//     try {
+//         // const id = ObjectId(data.id)
+//         const user = await User.findById(data.id)
+//         if (!user) {
+//             return NextResponse.json({ error: "User not found" }, { status: 404 });
+//         }
+//         return NextResponse.json(user)
+//     } catch (error) {
+//         console.log(error)
+//         return NextResponse.json({ error: "An error occurred" }, { status: 500 });
+//     }
 
-    try {
-        // const id = ObjectId(data.id)
-        const user = await User.findById(data.id)
-        if (!user) {
-            return NextResponse.json({ error: "User not found" }, { status: 404 });
-        }
-        return NextResponse.json(user)
-    } catch (error) {
-        console.log(error)
-        return NextResponse.json({ error: "An error occurred" }, { status: 500 });
-    }
-
-
-}
+// }
